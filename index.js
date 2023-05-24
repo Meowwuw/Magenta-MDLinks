e9ccc917d1043d03c9ac42b6d3d4267f75ed56a3
Magenta Paredes <paredesponcemagenta@gmail.com>
2023-05-24 11:19:13 -0500

    Enviando proyecto a github

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
-module.exports = () => {
-  // ...
+const fs = require('fs');
+
+function mdLinks(path, options) {
+  if (!fs.existsSync(path)) {
+    throw new Error('La ruta especificada no existe.');
+  }
+
+  const stats = fs.statSync(path);
+  if (stats.isFile()) {
+    // La ruta es un archivo
+  } else if (stats.isDirectory()) {
+    // La ruta es un directorio
+  }
+}
+
+const mdLinks = require('./mdLinks');
+
+// Ejemplo de uso
+const path = 'ruta/al/archivo-o-directorio';
+const options = {
+  // opciones si las hay
 };
+
+try {
+  const result = mdLinks(path, options);
+  console.log(result);
+} catch (error) {
+  console.error(error.message);
+}
+
+module.exports = mdLinks;
+
